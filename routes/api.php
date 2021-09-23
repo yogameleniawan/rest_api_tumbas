b045b472524f7f9f25c583112e84b03afafab40d
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2021-09-23 19:33:24 +0700

    update api

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('task', ToDoListController::class);
+Route::prefix('v1')->group(function () {
+    Route::apiResource('task', ToDoListController::class);
+});
